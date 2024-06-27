410ccda6753cdd6fd16f81f4b777c658ce5ffb1f
y-takagiiii <takagiyuri0712@gmail.com>
2024-06-27 10:35:27 +0900

    app.cssの読み込み

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import type { LinksFunction } from "@remix-run/node";
 import {
   Form,
   Links,
@@ -5,10 +6,15 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import appStylesHref from "./app.css"; // app.cssの読み込み
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+]; // links関数で定義したものが<Links />に入る
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
